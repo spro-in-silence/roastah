a162d24d02bffcb0c692ac40ecd256530f8d15b0
awinragoth <awinash@gmail.com>
2025-07-07 08:01:05 -0400

    login/logout

--- server/storage.ts
+++ server/storage.ts
@@ -189,7 +189,7 @@ export class DatabaseStorage implements IStorage {
   }
 
   async getUserByEmail(email: string): Promise<User | undefined> {
-    const [user] = await db.select().from(users).where(eq(users.email, email));
+    const [user] = await getDb().select().from(users).where(eq(users.email, email));
     return user;
   }
 
